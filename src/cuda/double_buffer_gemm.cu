62d9970d6ba07d46f9b4efb82cb72abd76dbee79
Pzzzzz <hello-cd.plus@hotmail.com>
2022-10-10 16:28:01 +0800

    update z map, new shared mem, and double buffer

--- src/cuda/double_buffer_gemm.cu
+++ src/cuda/double_buffer_gemm.cu
@@ -2,15 +2,12 @@
 #include <cuda_runtime.h>
 #include <algorithm>
 #include <vector>
-#include <cooperative_groups/memcpy_async.h>
-#include <cuda/pipeline>
-
 #ifndef __CUDACC__
 #include "cuda_runtime.h"
 #include "device_launch_parameters.h"
 void __syncthreads(); // workaround __syncthreads warning
+void __syncwarp();
 #endif
-
 #include <iostream>
 constexpr size_t BLOCK_SIZE = 16; // we assume that every block has equal blockDim.x and blockDim.y
 constexpr size_t BLOCK_M = 128;   // These const values decide how many thing a thread compute and the amount of shared memory to allocate.
@@ -50,47 +47,49 @@ __global__ void matrixMul(const float *A, const float *B, float *C,
     constexpr size_t threadsNum = BLOCK_SIZE * BLOCK_SIZE;
 
     float c[BLOCK_M_COMPUTE * BLOCK_N_COMPUTE] = {};
+    constexpr size_t subAlda = BLOCK_M + 4; // plus 4 here to avoid bank conflict and maintain float4 read
 
-    const float *baseA = A + baseX * K;
-    const float *baseB = B + baseY;
-
-    int colA = baseIdx >> 1, colB = baseIdx >> 5, rowA = (baseIdx & 1) << 2, rowB = (baseIdx << 2) & 127;
-    int warpId = baseIdx >> 5, warpBaseId = baseIdx & 31;
-    int colC = ((warpId >> 1 << 2) + (warpBaseId & 3)) << 3, rowC = (((warpId & 1) << 3) + (warpBaseId >> 2)) << 3;
-    float *baseC = C + (baseX + colC) * N + baseY + rowC;
-
-    __shared__ float subA[2][BLOCK_M * BLOCK_K];
+    __shared__ float subA[2][subAlda * BLOCK_K];
     __shared__ float subB[2][BLOCK_N * BLOCK_K];
 
     float4 regB[BLOCK_M_COMPUTE / 4]; // hopefully, these should reside in register.
     float4 regA[BLOCK_M_COMPUTE / 4];
 
+    const float *baseA = A + baseY * K;
+    const float *baseB = B + baseX;
+
+    auto compute_stage_idx = 0;
+
+    int rowA = baseIdx >> 1, rowB = baseIdx >> 5, colA = (baseIdx & 1) << 2, colB = (baseIdx << 2) & 127;
+    int warpId = baseIdx >> 5, warpBaseId = baseIdx & 31;
+    int rowC = ((warpId >> 1 << 3) + ((warpBaseId >> 4) << 1) + (warpBaseId & 1)) << 2, colC = (((warpId & 1) << 4) + ((warpBaseId & 15) >> 1)) << 2;
+    float *baseC = C + (baseY + rowC) * N + baseX + colC;
+
     float4 preA, preB;
 
-    size_t compute_stage_idx = 0;
-
-    preA = *reinterpret_cast<const float4 *>(baseA + colA * K + rowA);
-    *reinterpret_cast<float4 *>(&subB[0][baseIdx * 4]) = *reinterpret_cast<const float4 *>(baseB + colB * N + rowB);
-    subA[0][colA + rowA * BLOCK_M] = preA.x;
-    subA[0][colA + (rowA + 1) * BLOCK_M] = preA.y;
-    subA[0][colA + (rowA + 2) * BLOCK_M] = preA.z;
-    subA[0][colA + (rowA + 3) * BLOCK_M] = preA.w;
+    preB = *reinterpret_cast<const float4 *>(baseB + rowB * N + colB);
+    preA = *reinterpret_cast<const float4 *>(baseA + rowA * K + colA);
+    *reinterpret_cast<float4 *>(&subB[0][baseIdx * 4]) = preB;
+    subA[0][rowA + colA * subAlda] = preA.x;
+    subA[0][rowA + (colA + 1) * subAlda] = preA.y;
+    subA[0][rowA + (colA + 2) * subAlda] = preA.z;
+    subA[0][rowA + (colA + 3) * subAlda] = preA.w;
 
     __syncthreads();
 
     for (int i = BLOCK_K; i < K; i += BLOCK_K)
     {
-        preA = *reinterpret_cast<const float4 *>(baseA + i + colA * K + rowA);
-        preB = *reinterpret_cast<const float4 *>(baseB + i * N + colB * N + rowB);
+        preB = *reinterpret_cast<const float4 *>(baseB + i * N + rowB * N + colB);
+        preA = *reinterpret_cast<const float4 *>(baseA + i + rowA * K + colA);
 
 #pragma unroll
         for (int ii = 0; ii < BLOCK_K; ii++)
         {
-            regA[0] = *reinterpret_cast<float4 *>(&subA[compute_stage_idx][colC + ii * BLOCK_M]);
-            regA[1] = *reinterpret_cast<float4 *>(&subA[compute_stage_idx][(colC + 4) + ii * BLOCK_M]);
+            regB[0] = *reinterpret_cast<float4 *>(&subB[compute_stage_idx][colC + BLOCK_N * ii]);
+            regB[1] = *reinterpret_cast<float4 *>(&subB[compute_stage_idx][colC + 32 + BLOCK_N * ii]);
 
-            regB[0] = *reinterpret_cast<float4 *>(&subB[compute_stage_idx][rowC + BLOCK_N * ii]);
-            regB[1] = *reinterpret_cast<float4 *>(&subB[compute_stage_idx][rowC + 4 + BLOCK_N * ii]);
+            regA[0] = *reinterpret_cast<float4 *>(&subA[compute_stage_idx][rowC + ii * subAlda]);
+            regA[1] = *reinterpret_cast<float4 *>(&subA[compute_stage_idx][(rowC + 16) + ii * subAlda]);
 
 #pragma unroll
             for (int cpi = 0; cpi < BLOCK_M_COMPUTE / 4; cpi++)
@@ -120,23 +119,25 @@ __global__ void matrixMul(const float *A, const float *B, float *C,
                 }
             }
         }
+
         compute_stage_idx ^= 1;
+
         *reinterpret_cast<float4 *>(&subB[compute_stage_idx][baseIdx * 4]) = preB;
-        subA[compute_stage_idx][colA + rowA * BLOCK_M] = preA.x;
-        subA[compute_stage_idx][colA + (rowA + 1) * BLOCK_M] = preA.y;
-        subA[compute_stage_idx][colA + (rowA + 2) * BLOCK_M] = preA.z;
-        subA[compute_stage_idx][colA + (rowA + 3) * BLOCK_M] = preA.w;
+        subA[compute_stage_idx][rowA + colA * subAlda] = preA.x;
+        subA[compute_stage_idx][rowA + (colA + 1) * subAlda] = preA.y;
+        subA[compute_stage_idx][rowA + (colA + 2) * subAlda] = preA.z;
+        subA[compute_stage_idx][rowA + (colA + 3) * subAlda] = preA.w;
         __syncthreads();
     }
 
 #pragma unroll
     for (int ii = 0; ii < BLOCK_K; ii++)
     {
-        regA[0] = *reinterpret_cast<float4 *>(&subA[compute_stage_idx][colC + ii * BLOCK_M]);
-        regA[1] = *reinterpret_cast<float4 *>(&subA[compute_stage_idx][(colC + 4) + ii * BLOCK_M]);
+        regB[0] = *reinterpret_cast<float4 *>(&subB[compute_stage_idx][colC + BLOCK_N * ii]);
+        regB[1] = *reinterpret_cast<float4 *>(&subB[compute_stage_idx][colC + 32 + BLOCK_N * ii]);
 
-        regB[0] = *reinterpret_cast<float4 *>(&subB[compute_stage_idx][rowC + BLOCK_N * ii]);
-        regB[1] = *reinterpret_cast<float4 *>(&subB[compute_stage_idx][rowC + 4 + BLOCK_N * ii]);
+        regA[0] = *reinterpret_cast<float4 *>(&subA[compute_stage_idx][rowC + ii * subAlda]);
+        regA[1] = *reinterpret_cast<float4 *>(&subA[compute_stage_idx][(rowC + 16) + ii * subAlda]);
 
 #pragma unroll
         for (int cpi = 0; cpi < BLOCK_M_COMPUTE / 4; cpi++)
@@ -168,17 +169,36 @@ __global__ void matrixMul(const float *A, const float *B, float *C,
     }
 
 #pragma unroll
-    for (int i = 0; i < BLOCK_M_COMPUTE; i++)
-#pragma unroll
-        for (int j = 0; j < BLOCK_N_COMPUTE; j += 4)
-        {
-            preA = *reinterpret_cast<float4 *>(&baseC[i * N + j]);
-            c[i * BLOCK_M_COMPUTE + j] = c[i * BLOCK_M_COMPUTE + j] * alpha + preA.x * beta;
-            c[i * BLOCK_M_COMPUTE + j + 1] = c[i * BLOCK_M_COMPUTE + j + 1] * alpha + preA.y * beta;
-            c[i * BLOCK_M_COMPUTE + j + 2] = c[i * BLOCK_M_COMPUTE + j + 2] * alpha + preA.z * beta;
-            c[i * BLOCK_M_COMPUTE + j + 3] = c[i * BLOCK_M_COMPUTE + j + 3] * alpha + preA.w * beta;
-            *reinterpret_cast<float4 *>(&baseC[i * N + j]) = *reinterpret_cast<float4 *>(&c[i * BLOCK_M_COMPUTE + j]);
-        }
+    for (int i = 0; i < 4; i++)
+    {
+        *reinterpret_cast<float4 *>(&regA[0]) = *reinterpret_cast<float4 *>(&baseC[i * N]);
+        regA[0].x = regA[0].x * beta + alpha * c[i * BLOCK_N_COMPUTE];
+        regA[0].y = regA[0].y * beta + alpha * c[1 + i * BLOCK_N_COMPUTE];
+        regA[0].z = regA[0].z * beta + alpha * c[2 + i * BLOCK_N_COMPUTE];
+        regA[0].w = regA[0].w * beta + alpha * c[3 + i * BLOCK_N_COMPUTE];
+        *reinterpret_cast<float4 *>(&baseC[i * N]) = *reinterpret_cast<float4 *>(&regA[0]);
+
+        *reinterpret_cast<float4 *>(&regA[0]) = *reinterpret_cast<float4 *>(&baseC[i * N + 32]);
+        regA[0].x = regA[0].x * beta + alpha * c[4 + i * BLOCK_N_COMPUTE];
+        regA[0].y = regA[0].y * beta + alpha * c[5 + i * BLOCK_N_COMPUTE];
+        regA[0].z = regA[0].z * beta + alpha * c[6 + i * BLOCK_N_COMPUTE];
+        regA[0].w = regA[0].w * beta + alpha * c[7 + i * BLOCK_N_COMPUTE];
+        *reinterpret_cast<float4 *>(&baseC[i * N + 32]) = *reinterpret_cast<float4 *>(&regA[0]);
+
+        *reinterpret_cast<float4 *>(&regA[0]) = *reinterpret_cast<float4 *>(&baseC[(i + 16) * N]);
+        regA[0].x = regA[0].x * beta + alpha * c[32 + i * BLOCK_N_COMPUTE];
+        regA[0].y = regA[0].y * beta + alpha * c[33 + i * BLOCK_N_COMPUTE];
+        regA[0].z = regA[0].z * beta + alpha * c[34 + i * BLOCK_N_COMPUTE];
+        regA[0].w = regA[0].w * beta + alpha * c[35 + i * BLOCK_N_COMPUTE];
+        *reinterpret_cast<float4 *>(&baseC[(i + 16) * N]) = *reinterpret_cast<float4 *>(&regA[0]);
+
+        *reinterpret_cast<float4 *>(&regA[0]) = *reinterpret_cast<float4 *>(&baseC[(i + 16) * N + 32]);
+        regA[0].x = regA[0].x * beta + alpha * c[36 + i * BLOCK_N_COMPUTE];
+        regA[0].y = regA[0].y * beta + alpha * c[37 + i * BLOCK_N_COMPUTE];
+        regA[0].z = regA[0].z * beta + alpha * c[38 + i * BLOCK_N_COMPUTE];
+        regA[0].w = regA[0].w * beta + alpha * c[39 + i * BLOCK_N_COMPUTE];
+        *reinterpret_cast<float4 *>(&baseC[(i + 16) * N + 32]) = *reinterpret_cast<float4 *>(&regA[0]);
+    }
 }
 
 void sgemm(int M, int N, int K, float *a, float *b, float *c, float alpha = 1, float beta = 0)
